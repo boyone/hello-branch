52df81147af2aa9f842af725d8036cc7d566e7ca
boyone <boyone.g@gmail.com>
2021-11-26 10:50:03 +0700

    Add api/v2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -12,10 +13,27 @@ func main() {
 
 	router.Get("/api/v1/greeting", HelloText)
 
+	router.Post("/api/v2/greeting", HelloJSON)
+
 	http.ListenAndServe(":3000", router)
 }
 
 func HelloText(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
 	fmt.Fprintf(w, "Hello %s", name)
+}
+
+func HelloJSON(w http.ResponseWriter, r *http.Request) {
+	name := struct {
+		Name string `json:"name,omitempty"`
+	}{}
+	json.NewDecoder(r.Body).Decode(&name)
+
+	greeting := struct {
+		Message string `json:"message,omitempty"`
+	}{fmt.Sprintf("Hello %s", name.Name)}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&greeting)
 }
