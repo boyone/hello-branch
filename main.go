9e471240164a76eeac13610452a8c5825d49a824
tuddaw <tuddaw@Izpal.com>
2021-11-26 10:39:16 +0700

    hellotext

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 func main() {
 	router := chi.NewRouter()
 
-	router.Get("/api/v1/greeting", func(w http.ResponseWriter, r *http.Request) {
-		name := r.FormValue("name")
-		fmt.Fprintf(w, "Hello %s", name)
-	})
+	router.Get("/api/v1/greeting", HelloText)
 
 	http.ListenAndServe(":3000", router)
+}
+
+func HelloText(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	fmt.Fprintf(w, "Hello %s", name)
 }
